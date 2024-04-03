bada6eb5bdd08c3adb2a6c6bd25158d62a96dc6a
soumya723457 <soumyaaitmbark@gmail.com>
2024-04-03 00:05:53 +0000

    first commit

---
+++ src/main/java/com/example/spring_mvc_thylemeaf_hospital/SpringMvcThylemeafHospitalApplication.java
@@ -0,0 +1,49 @@
+package com.example.spring_mvc_thylemeaf_hospital;
+
+import com.example.spring_mvc_thylemeaf_hospital.entities.Patient;
+import com.example.spring_mvc_thylemeaf_hospital.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class SpringMvcThylemeafHospitalApplication implements CommandLineRunner {
+    @Autowired
+  private PatientRepository patientRepository;
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringMvcThylemeafHospitalApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        /*//en utilisant constructeur sans paramètre
+        Patient patient=new Patient();
+        patient.setId(null);
+        patient.setNom("Mohammed");
+        patient.setDateNaissance(new Date());
+        patient.setMalade(false);
+        patient.setScore(23);
+
+        //Constructeur avec paramètre
+        Patient patient1=new Patient(null,"hicham",new Date(),false,123);
+        //en utilisant Builder
+        Patient patient2=Patient.builder()
+                .nom("imane")
+                .dateNaissance(new Date())
+                .score(65)
+                .malade(true)
+                .build();*/
+
+
+        patientRepository.save(new Patient(null,"Mohammed",new Date(),false,25));
+        patientRepository.save(new Patient(null,"ilyas",new Date(),true,2));
+        patientRepository.save(new Patient(null,"oumama",new Date(),false,12));
+        patientRepository.save(new Patient(null,"othmane",new Date(),true,10));
+
+
+    }
+}
---
+++ src/main/java/com/example/spring_mvc_thylemeaf_hospital/web/PatientController.java
@@ -0,0 +1,53 @@
+package com.example.spring_mvc_thylemeaf_hospital.web;
+
+import com.example.spring_mvc_thylemeaf_hospital.entities.Patient;
+import com.example.spring_mvc_thylemeaf_hospital.repositories.PatientRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class PatientController {
+    private PatientRepository patientRepository;
+    @GetMapping("/index")
+    //@RequestParam(name="page") récupèrer un paramètre qui s'appelle page est affecter au param page au dessous
+    public String index(Model model,
+                        @RequestParam(name = "page",defaultValue ="0" ) int page,
+                        @RequestParam(name = "size",defaultValue = "4") int size,
+                        @RequestParam(name = "keyword",defaultValue = "") String kw
+                        ){
+        /* HttpServletRequest request*/
+        /*int page=Integer.parseInt(request.getParameter("page"));
+        int size=Integer.parseInt(request.getParameter("size"));*/
+
+      /* List<Patient> patientList=patientRepository.findAll();
+       model.addAttribute("listPatient",patientList);*/
+
+        //Page<Patient> pagePatients=patientRepository.findAll(PageRequest.of(page,size));
+        Page<Patient> pagePatients=patientRepository.findByNomContains(kw,PageRequest.of(page,size));
+
+
+        model.addAttribute("listPatients",pagePatients.getContent());;
+        //return le nombre total des pages
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("keyword",kw);
+
+        return "patients";
+
+    }
+    @GetMapping("/delete")
+    public String delete(Long id,String keyword,int page){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+ "&keyword="+keyword;
+    }
+}
